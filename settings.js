febddf921a698aaa059a21cfe3b156bca499a458
Weliam10 <weliam810@gmail.com>
2024-08-14 02:24:02 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,8 +36,8 @@ global.location = "Africa, Gabon, Libreville" //ur location
 
 //new
 global.botname = '𝐓𝐎𝐆𝐄-𝐌𝐃' //ur bot name
-global.ownernumber = ['24102150169'] //ur owner number, dont add more than one
-global.ownername = '𓆩𝐓𝐎𝐆𝐄𓆪 ✇ ◤✞𝐈𝐍𝐔𝐌𝐀𝐊𝐈' //ur owner name
+global.ownernumber = ['27645340967'] //ur owner number, dont add more than one
+global.ownername = '𓆩Valtrix𓆪✞' //ur owner name
 global.websitex = "https://toge-md-web.onrender.com"
 global.wagc = "https://whatsapp.com/channel/0029VaiuD4s4IBhI0fzbv40Z"
 global.themeemoji = '🛠️'
@@ -45,9 +45,9 @@ global.wm = "𝐓𝐎𝐆𝐄_𝐁𝐎𝐓 𝐈𝐧𝐜 ✨"
 global.botscript = 'https://github.com/toge012345/TOGE-MD' //script link
 global.packname = "Sticker By"
 global.author = "𓆩𝐓𝐎𝐆𝐄𓆪 ✇ ◤✞𝐈𝐍𝐔𝐌𝐀𝐊𝐈"
-global.creator = "24102150169@s.whatsapp.net"
+global.creator = "27645340967@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["24105114159"] // Premium User
+global.premium = ["27645340967"] // Premium User
 
 //channel id
 global.xchannel = {
